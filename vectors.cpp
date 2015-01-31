e07c0a7ab6ba588973c7ebcc67203c9570887389
Luiz Branco <me@luizbranco.com>
2015-01-31 13:36:06 -0500

    Add escalar multiplication

--- vectors.cpp
+++ vectors.cpp
@@ -28,6 +28,15 @@ operator-(vector2 a, vector2 b) {
   return result;
 }
 
+inline vector2
+operator*(vector2 a, real32 b) {
+  vector2 result = {
+    a.x * b,
+    a.y * b
+  };
+  return result;
+}
+
 inline real32
 dot_product(vector2 a, vector2 b) {
   return (a.x * b.x) + (a.y * b.y);
@@ -50,6 +59,7 @@ int main() {
   print_vector("A+B", a+b);
   print_vector("A-B", a-b);
   print_vector("B-A", b-a);
+  print_vector("A*2", a * 2.0f);
   printf("A.B: %.2f\n", dot_product(a,b));
   printf("A length: %.2f\n", length(a));
   return 0;
